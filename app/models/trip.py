59f6da8381e500de43d5bea4b00a9d955d67f722
Darmendel <mendelowitz10@gmail.com>
2025-10-31 16:31:17 +0200

    Connecting to PostgreSQL and creating unit tests

--- app/models/trip.py
+++ app/models/trip.py
@@ -1,18 +1,18 @@
-# app/models/trip.py
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base
 from app.db.types import StringList
 
 Base = declarative_base()
 
-class TripModel(Base):
+# ORM Model
+class Trip(Base):
     __tablename__ = "trips"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String(100), nullable=False)
     start_date = Column(Date, nullable=False)
     end_date = Column(Date, nullable=False)
-    destinations = Column(String, nullable=False)  # Storing as comma-separated string *for now*
+    destinations = Column(String(100), nullable=False)  # Storing as comma-separated string *for now*
     # destinations = Column(StringList, nullable=False)
 
     def __repr__(self):
